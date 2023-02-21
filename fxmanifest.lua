ec825e4cab2115de05e2b4e4330b49a80dc992df
Guudz <Kilian1702@outlook.com>
2023-02-21 15:20:08 +0100

    Initial commit

---
+++ fxmanifest.lua
@@ -0,0 +1,16 @@
+fx_version 'adamant'
+game 'gta5'
+
+this_is_a_map 'yes'
+
+file 'gabz_timecycle_mods_1.xml'
+data_file 'TIMECYCLEMOD_FILE' 'gabz_timecycle_mods_1.xml'
+
+client_script {
+  "main.lua"
+}
+
+server_scripts {
+  "@mysql-async/lib/MySQL.lua",
+  "sync/genpi1"
+}
